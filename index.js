a0e4ca45e94e8732374f7882b30826f2a903c088
Ghalistan <gghalistan@gmail.com>
2021-04-11 12:26:45 +0700

    node index.js [email] [password]

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Builder, By, Key } = require('selenium-webdriver');
+const { Builder, By, Key, until } = require('selenium-webdriver');
 (async function example() {
   require('chromedriver')
   let chrome = require('selenium-webdriver/chrome')
@@ -9,6 +9,7 @@ const { Builder, By, Key } = require('selenium-webdriver');
       new chrome.Options()
         .addArguments(
           [
+            // "--headless",
             "--disable-notifications",
             "--disable-infobars",
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"
@@ -22,11 +23,12 @@ const { Builder, By, Key } = require('selenium-webdriver');
     await driver.get('http://www.facebook.com')
     await driver
       .findElement(By.name('email'))
-      .sendKeys('qohqwrk682@noquviti.com')
-    await driver.findElement(By.name('pass')).sendKeys('Aku999@', Key.RETURN)
+      .sendKeys(process.argv[2])
+    await driver.findElement(By.name('pass')).sendKeys(process.argv[3], Key.RETURN)
     await setTimeout(() => {
       driver.get('https://www.facebook.com/100008720376125/videos/2645872805713373/')
-      driver.findElement(By.xpath(`//div[@aria-label="Putar video"]`)).click()
+      driver.wait(until.elementLocated(By.xpath(`//div[@aria-label="Putar video" or @aria-label="Play video"]`)))
+      driver.findElement(By.xpath(`//div[@aria-label="Putar video" or @aria-label="Play video"]`)).click()
     }, 5000)
     // await driver.executeScript(
     //   'document.getElementsByTagName("video")[0].play()',
